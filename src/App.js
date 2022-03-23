756a1bb2ff7dfe76ee1a09038a24c7afcbf7ef67
bruhu <annabru.abh@gmail.com>
2022-03-23 16:03:08 +0100

    removed all test code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
 function App() {
-  const comments = [
-    { id: 1, text: 'one' },
-    { id: 2, text: 'two' },
-    { id: 3, text: 'three' },
-  ]
-
-  const loading = false
-  const showComments = true
-  const commentBlock = (
-    <div className='comments'>
-      <h3>Comments ({comments.length})</h3>
-
-      <ul></ul>
-      {comments.map((comment, index) => (
-        <li key={index}>Comment {comment.text}</li>
-      ))}
-    </div>
-  )
-
-  if (loading) return <p>Loading...</p>
-
   return (
     <div className='container'>
       <h1>My Feedback App</h1>
-
-      {showComments && commentBlock}
     </div>
   )
 }
