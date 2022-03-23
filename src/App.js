6340483b88cc7230b9ebbe1ada24fdab8a40c1fe
bruhu <annabru.abh@gmail.com>
2022-03-23 15:37:40 +0100

    rendered list using basic map function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 function App() {
-  return <h1>Hello from the App Component</h1>
+  const comments = [
+    { id: 1, text: 'one' },
+    { id: 2, text: 'two' },
+    { id: 3, text: 'three' },
+  ]
+  return (
+    <div className='container'>
+      <h1>My Feedback App</h1>
+      <p>Here's a paragraph - did it solve the issue?</p>
+
+      <div className='comments'>
+        <h3>Comments ({comments.length})</h3>
+
+        <ul></ul>
+        {comments.map((comment, index) => (
+          <li key={index}>Comment {comment.text}</li>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default App
