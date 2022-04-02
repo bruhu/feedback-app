2e173c2115830bdc7e15ddbc6832b06b7f8117f8
bruhu <annabru.abh@gmail.com>
2022-04-02 18:13:58 +0200

    set & displayed state in FeedbackItem Component using setState hook

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react"
+
 function FeedbackItem() {
+const [rating, setRating] = useState(7)
+
   return (
     <div className="card">
-      <div className="num-display">10</div>
+      <div className="num-display">{rating}</div>
       <div className="text-display">
         This is an example of a feedback item.
       </div>
