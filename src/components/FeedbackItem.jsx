b8fdfc2ec0589f72eec4f7e77c56f7386a53ed31
bruhu <annabru.abh@gmail.com>
2022-04-02 18:15:44 +0200

    displayed text through state as well

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,13 +2,12 @@ import { useState } from "react"
 
 function FeedbackItem() {
 const [rating, setRating] = useState(7)
+const [text, setText] = useState('This is an example of a feedback item.')
 
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
-      <div className="text-display">
-        This is an example of a feedback item.
-      </div>
+      <div className="text-display">{text}</div>
     </div>
   )
 }
