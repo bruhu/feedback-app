ded663e226aa8371ba4c6a0015a1ea173829501b
bruhu <annabru.abh@gmail.com>
2022-04-04 17:14:54 +0200

    set global state in app.js with sample data from original repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackItem from './components/FeedbackItem'
 
 function App() {
+  const [feedback, setFeedback] = useState(
+    {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    }
+  )
   return (
     <>
       <Header />
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,10 +4,23 @@ function FeedbackItem() {
 const [rating, setRating] = useState(7)
 const [text, setText] = useState('This is an example of a feedback item.')
 
+const handleClick = () => {
+  setRating(89)
+}
+
+  // using this instead of just the value for new state allows you to access previous state
+const handleClickyClick = () => {
+  setRating((prev) => {
+    return prev + 1
+  })
+}
+
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
+      <button onClick={handleClick}>Update rating</button>
+      <button onClick={handleClickyClick}>Rating + 1</button>
     </div>
   )
 }
