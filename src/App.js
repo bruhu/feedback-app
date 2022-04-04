dbab9596221e687e50b0f5465855e17e8dab65e5
bruhu <annabru.abh@gmail.com>
2022-04-04 17:16:51 +0200

    small refactor: added /data and FeedbackData.js with the sample data to avoid code cluttering

--- src/App.js
+++ src/App.js
@@ -3,23 +3,7 @@ import Header from './components/Header'
 import FeedbackItem from './components/FeedbackItem'
 
 function App() {
-  const [feedback, setFeedback] = useState(
-    {
-      id: 1,
-      rating: 10,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-    },
-    {
-      id: 2,
-      rating: 9,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-    },
-    {
-      id: 3,
-      rating: 8,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-    }
-  )
+  const [feedback, setFeedback] = useState()
   return (
     <>
       <Header />
