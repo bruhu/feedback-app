61227f469c5181b61ea31f1340faba050c24b8a2
bruhu <annabru.abh@gmail.com>
2022-04-07 11:06:31 +0200

    set state as input value

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -14,7 +14,7 @@ function FeedbackForm() {
         <h2>How would you rate your experience with us?</h2>
         {/* @todo - rating select component */}
         <div className='input-group'>
-          <input onChange={handleTextChange} type='text' placeholder='write a review' />
+          <input onChange={handleTextChange} type='text' placeholder='Write a review' value={text} />
           <button type='submit'>Send</button>
         </div>
       </form>
