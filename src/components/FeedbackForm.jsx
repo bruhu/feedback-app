7cb9359538794921ed6cc51df8522893b0bd3938
bruhu <annabru.abh@gmail.com>
2022-04-07 11:05:06 +0200

    added function to set state w/ input field text value

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,14 +3,18 @@ import Card from './shared/Card'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
-  
+
+  const handleTextChange = (e) => {
+    setText(e.target.value)
+  }
+
   return (
     <Card>
       <form>
         <h2>How would you rate your experience with us?</h2>
         {/* @todo - rating select component */}
         <div className='input-group'>
-          <input type='text' placeholder='write a review' />
+          <input onChange={handleTextChange} type='text' placeholder='write a review' />
           <button type='submit'>Send</button>
         </div>
       </form>
