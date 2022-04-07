d4b3f98e172aa98ccfc7bac09d97683f01f96418
bruhu <annabru.abh@gmail.com>
2022-04-07 10:58:34 +0200

    added input field and button for feedback submission

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,4 +1,4 @@
-import Card from "./shared/Card"
+import Card from './shared/Card'
 
 function FeedbackForm() {
   return (
@@ -6,6 +6,10 @@ function FeedbackForm() {
       <form>
         <h2>How would you rate your experience with us?</h2>
         {/* @todo - rating select component */}
+        <div className='input-group'>
+          <input type='text' placeholder='write a review' />
+          <button type='submit'>Send</button>
+        </div>
       </form>
     </Card>
   )
