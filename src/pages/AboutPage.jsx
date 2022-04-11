994888fae8bdcfa11f0f60a557fe59c78272349a
bruhu <annabru.abh@gmail.com>
2022-04-11 14:22:36 +0200

    wrapped AboutPage content in Card, added basic info and version number

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,15 @@
+import Card from '../components/shared/Card'
+
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <Card>
+      <div className='about'>
+        <h1>About the Project</h1>
+        <p>This is a React app to leave product feedback</p>
+        <p>v1.0.0</p>
+        <p href='/'></p>
+      </div>
+    </Card>
   )
 }
 
