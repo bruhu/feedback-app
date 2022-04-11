01213cacdcc4677933555b3515c244bdecee863d
bruhu <annabru.abh@gmail.com>
2022-04-11 13:03:00 +0200

    added very simple fade animation for adding / deleting feedback items

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-// import {} from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
@@ -7,15 +7,35 @@ function FeedbackList({feedback, handleDelete}) {
   if(!feedback || feedback.length === 0) {
     return(<p>There's no feedback yet.</p>)
   } 
-
+  
   return (
     <div className='feedback-list'>
+      <AnimatePresence>
     {feedback.map((item) => (
+      <motion.div 
+        key={item.id}
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        exit={{opacity: 0}}      
+      >
       <FeedbackItem key={item.id} item={item} 
       handleDelete={handleDelete}/>
+      </motion.div>
     ))}
+    
+      </AnimatePresence>
     </div>
   )
+
+  // version without animation
+  // return (
+  //   <div className='feedback-list'>
+  //   {feedback.map((item) => (
+  //     <FeedbackItem key={item.id} item={item} 
+  //     handleDelete={handleDelete}/>
+  //   ))}
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
