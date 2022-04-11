9f03d2667a78db2ea45a86e0502c1eb7aaca0338
bruhu <annabru.abh@gmail.com>
2022-04-11 16:19:25 +0200

    typo

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,7 +5,7 @@ function AboutPage() {
     <Card>
       <div className='about'>
         <h1>About the Project</h1>
-        <p>This is a React app to leave product feedback</p>
+        <p>This is a React app to leave product and service feedback</p>
         <p>v1.0.0</p>
         <p>
           <a href='/'>Take me back to the home page</a>
