0a31b9f158f2aac2978cf0299af6d2ddd1e2b1a1
bruhu <annabru.abh@gmail.com>
2022-04-13 13:28:39 +0200

    set state and provider in FeedbackContext

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,24 @@
+import { createContext, useState } from 'react'
+
+const FeedbackContext = createContext()
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: 'This item comes from FeedbackContext'
+    }
+  ])
+  return (
+    <FeedbackContext.Provider
+      value={
+        {
+          // everything we want to pass in
+        }
+      }
+    >
+      {/* children = all the components that will access our context */}
+      {children}
+    </FeedbackContext.Provider>
+  )
+}
