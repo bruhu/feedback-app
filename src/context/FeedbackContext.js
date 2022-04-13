065af5b4bef11d311bed55e6bf4c118fca61fea1
bruhu <annabru.abh@gmail.com>
2022-04-13 14:17:56 +0200

    added more hardcoded feedback items for feature testing

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -7,8 +7,18 @@ export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This item comes from FeedbackContext',
+      text: 'This is feedback item 1',
       rating: 10,
+    },
+    {
+      id: 2,
+      text: 'This is feedback item 2',
+      rating: 6,
+    },
+    {
+      id: 3,
+      text: 'This is feedback item 3',
+      rating: 8,
     },
   ])
 
