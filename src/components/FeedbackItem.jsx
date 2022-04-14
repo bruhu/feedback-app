0c255c4037b170697fb64e11c8dce5a99f647ae6
bruhu <annabru.abh@gmail.com>
2022-04-14 14:58:45 +0200

    added edit icon

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,4 +1,4 @@
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import { useContext } from 'react'
 import PropTypes from 'prop-types'
 import FeedbackContext from '../context/FeedbackContext'
@@ -11,6 +11,9 @@ function FeedbackItem({item}) {
       <div className='num-display'>{item.rating}</div>
       <button className='close'>
         <FaTimes onClick={() => deleteFeedback(item.id)} color='purple'/>
+      </button>
+      <button className='edit'>
+        <FaEdit color='purple'></FaEdit>
       </button>
       <div className='text-display'>{item.text}</div>
     </Card>
